d2b61996ad03422a858168da1750ded96b72bfb5
kweyaanton <kweyaanton@gmail.com>
2024-06-21 12:18:36 +0300

    updated profiles

--- lib/app/modules/create_profile/controller/create_profile_controller.dart
+++ lib/app/modules/create_profile/controller/create_profile_controller.dart
@@ -9,9 +9,13 @@ import 'package:ambu_go_user/helper/snackbar.dart';
 
 class CreateProfileController extends GetxController {
   TextEditingController name = TextEditingController();
+  TextEditingController gender = TextEditingController();
   TextEditingController address = TextEditingController();
-  TextEditingController city = TextEditingController();
   TextEditingController phoneNumber = TextEditingController();
+  TextEditingController city = TextEditingController();
+  TextEditingController bloodtype = TextEditingController();
+  TextEditingController emergencyContact = TextEditingController();
+
   final users = FirebaseFirestore.instance.collection('users');
   final prefs = SPController();
   onCreateProfile() async {
@@ -20,9 +24,12 @@ class CreateProfileController extends GetxController {
       await users.doc(prefs.getUserId()).set({
         'user Id':SPController().getUserId(),
         "name": name.text,
+        "gender": gender.text,
         "address": address.text,
         "phone":phoneNumber.text,
         "city":city.text,
+        "emergencyContact": emergencyContact.text,
+        "bloodtype": emergencyContact.text
       }).then((value) {
         LoadingUtils.hideLoader();
         AmbulanceGoApp.launch();
